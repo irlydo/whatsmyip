432a2eb4e64ec1766a5789e32480166f5e04d922
irlydo <fokum8@gmail.com>
2024-02-15 03:46:30 +0000

    .

--- components/Map.jsx
+++ components/Map.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { GoogleMap, Marker } from "@react-google-maps/api";
 
 const MapComponent = ({ selectedLocation }) => {
-  const [center, setCenter] = useState({ lat: -34.397, lng: 150.644 }); // Default center
+  const [center, setCenter] = useState({ lat: -34.397, lng: 150.644 });
 
   useEffect(() => {
     if (selectedLocation) {
@@ -11,13 +11,12 @@ const MapComponent = ({ selectedLocation }) => {
   }, [selectedLocation]);
 
   const mapContainerStyle = {
-    height: "100%", // Fill the height of the parent div
-    width: "100%", // Fill the width of the parent div
+    height: "100%",
+    width: "100%",
   };
 
   return (
     <div className="h-full w-full">
-      {/* Use Tailwind to ensure this div fills the height of its container */}
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         zoom={3}
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,8 @@ import "@/app/globals.css";
 import MapComponent from "@/components/Map";
 import { useState, useRef, useEffect } from "react";
 import { LoadScript } from "@react-google-maps/api";
+import { IoMdSearch } from "react-icons/io";
+import { RiUserSearchLine } from "react-icons/ri";
 
 export default function Home() {
   const [selectedLocation, setSelectedLocation] = useState(null);
@@ -13,13 +15,13 @@ export default function Home() {
 
   const fetchUserIpDetails = () => {
     if (ipInputRef.current) {
-      ipInputRef.current.value = ""; // Clear the input field
+      ipInputRef.current.value = "";
     }
 
     fetch("/api/getUserIpInfo")
       .then((response) => response.json())
       .then((data) => {
-        setIpInfo(data); // Set the IP info for display
+        setIpInfo(data);
         if (data.loc) {
           const [lat, lng] = data.loc.split(",");
           setSelectedLocation({ lat: parseFloat(lat), lng: parseFloat(lng) }); // Set the location for the map
@@ -32,6 +34,12 @@ export default function Home() {
   useEffect(() => {
     fetchUserIpDetails();
   }, []);
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleIPSearch();
+    }
+  };
 
   const handleIPSearch = () => {
     const ip = ipInputRef.current.value;
@@ -92,21 +100,22 @@ export default function Home() {
           )}
           <input
             ref={ipInputRef}
+            onKeyDown={handleKeyPress}
             type="text"
             placeholder="Enter IP Address..."
             className="p-2 w-80 text-black mt-4"
           />
           <button
             onClick={handleIPSearch}
-            className="mt-4 px-5 py-2 bg-blue-500 rounded hover:bg-blue-700 transition duration-300"
+            className="flex mt-4 px-5 py-2 bg-blue-500 rounded hover:bg-blue-700 transition duration-300"
           >
-            Search
+            Search <IoMdSearch className="ml-1 mt-1"/>
           </button>
           <button
             onClick={fetchUserIpDetails}
-            className="mt-4 px-5 py-2 bg-green-500 rounded hover:bg-green-700 transition duration-300"
+            className="flex mt-4 px-5 py-2 bg-green-500 rounded hover:bg-green-700 transition duration-300"
           >
-            My IP
+            My IP <RiUserSearchLine className="ml-1 mt-1"/>
           </button>
         </div>
         {/* Map Area - Right Side */}
